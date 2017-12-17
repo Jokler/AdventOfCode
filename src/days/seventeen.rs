f0eda792df2617ebf60bccae1cc1fd1169181cba
Jokler <jokler.contact@gmail.com>
2017-12-17 20:53:26 +0100

    Day Seventeen Part 2

--- src/days/seventeen.rs
+++ src/days/seventeen.rs
@@ -1,11 +1,9 @@
 use std::str::FromStr;
-use itertools::Itertools;
-use std::collections::HashMap;
 
 pub fn run(input: &str, puzzle: u8, debug: bool) -> String {
     match puzzle {
         1 => first(input, debug),
-        2 => String::from("I have not solved part 2 yet"),
+        2 => second(input, debug),
         _ => String::from("There are only 2 puzzles per day"),
     }
 }
@@ -29,4 +27,26 @@ fn first(input: &str, debug: bool) -> String {
 
     let len = vec.len();
     vec[(current + 1) % len].to_string()
+}
+
+fn second(input: &str, debug: bool) -> String {
+    if debug {
+        println!("No debug output for this puzzle");
+    }
+
+    let num = usize::from_str(input).unwrap();
+    let mut current = 0;
+    let mut result = 0;
+
+    for i in 1..(50_000_000 + 1) {
+        current = (current + num) % i;
+
+        current += 1;
+
+        if current == 1 {
+            result = i;
+        }
+    }
+
+    result.to_string()
 }
